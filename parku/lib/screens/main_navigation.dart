e5a932dd4d5580fdddf950fdd9aa6de27336486b
Camilafajardo22 <c.fajardos@uniandes.edu.co>
2026-09-18 21:32:47 -0500

    Clickeable parking marker in the map implementation

--- parku/lib/screens/main_navigation.dart
+++ parku/lib/screens/main_navigation.dart
@@ -258,6 +258,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
           onNavTap: changePage,
           hasActiveParking: hasActiveParking,
           onOpenMyParking: openMyParking,
+          onSelectParking: openParkingDetail,
         );
     }
   }
